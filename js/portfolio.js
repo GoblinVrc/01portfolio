25329543de94d7fb503e9f464e7958610fa4f4df
GoblinVrc <36779214+GoblinVrc@users.noreply.github.com>
2018-03-06 21:50:08 +0100

    Initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+var holder;
+
+function checkMe(){
+  holder = $(window).height()
+  console.log(holder);
+  holder = 0;
+};
+
+function positionMe(element){
+  var hold = element.attr()
+}
+
+var elementToPosition = $('#drop-text-container');
